3a6e4dc8f9a88bdc81f67ba8a92b87f43a640853
derek.berner <derek.berner@edointeractive.com>
2012-12-18 17:43:59 -0600

    Working on long poll

--- bevy-pro-frontend/src/main/java/com/bernerbits/vandy/bevypro/controller/BeverageMachineController.java
+++ bevy-pro-frontend/src/main/java/com/bernerbits/vandy/bevypro/controller/BeverageMachineController.java
@@ -1,37 +1,56 @@
 package com.bernerbits.vandy.bevypro.controller;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import com.bernerbits.vandy.bevypro.dao.BeverageDao;
 import com.bernerbits.vandy.bevypro.model.Beverage;
+import com.bernerbits.vandy.bevypro.model.BeverageMachineModel;
 
-@Controller
-public class BeverageMachineController {
+/**
+ * MVC Controller for Beverage Machine UI. <br/>
+ * 
+ * This controller is application-scoped, as the system architecture is such that
+ * individual session state need not be tracked. <br/>
+ * 
+ * However, it's possible for a user to generate race conditions by pressing buttons
+ * in rapid succession. Therefore, all controller actions are synchronized.<br/>
+ * 
+ * @author derekberner
+ */
+@Controller public class BeverageMachineController {
 
 	private HardwareController hardwareController;
 	private BeverageDao beverageDao;
 	private String message = "";
 	
-	public void setHardwareController(HardwareController hardwareController) {
+	public synchronized void setHardwareController(HardwareController hardwareController) {
 		this.hardwareController = hardwareController;
 	}
-	public void setBeverageDao(BeverageDao beverageDao) {
+	public synchronized void setBeverageDao(BeverageDao beverageDao) {
 		this.beverageDao = beverageDao;
 	}
 	
 	@RequestMapping(value="/index.ftl",produces="text/html")
-	public void populateModel(ModelMap model) {
-		model.put("bevs", hardwareController.check(beverageDao.getBeverages()));
-		model.put("credit", hardwareController.getCredit());
-		model.put("message", message);
+	public synchronized BeverageMachineModel getModel() {
+		BeverageMachineModel model = new BeverageMachineModel();
+		model.setBevs(hardwareController.check(beverageDao.getBeverages()));
+		model.setCredit(hardwareController.getCredit());
+		model.setMessage(message);
+		return model;
 	}
 
 	@RequestMapping(value="/dispense/{id}.ftl",produces="text/html")
-	public String dispense(@PathVariable("id") int beverageId) {
+	public synchronized String dispense(@PathVariable("id") int beverageId) {
 		Beverage beverage = beverageDao.getBeverage(beverageId);
 		hardwareController.check(beverage);
 		if(beverage.isSoldOut()) {
@@ -44,11 +63,12 @@ public class BeverageMachineController {
 			hardwareController.dispense(beverage);
 			message = "Vending...";
 		}
+		doModelUpdate();
 		return "redirect:/index.ftl";
 	}
 	
 	@RequestMapping(value="/refund.ftl",produces="text/html")
-	public String refund() {
+	public synchronized String refund() {
 		int credit = hardwareController.getCredit();
 		if(credit == 0) {
 			message = "No credit to refund.";
@@ -56,6 +76,46 @@ public class BeverageMachineController {
 			hardwareController.refund();
 			message = "Refunded " + credit + "&cent;.";
 		}
+		doModelUpdate();
 		return "redirect:/index.ftl";
 	}
+	
+	// Long polling support for model updates. 
+	
+	/**
+	 * Queue of asynchronous responses awaiting a model update.
+	 */
+	private final Queue<DeferredResult<BeverageMachineModel>> awaitingResults = new LinkedList<DeferredResult<BeverageMachineModel>>(); 
+
+	/**
+	 * Places an asynchronous response on the queue, defaulting to the current model if nothing changes in 
+	 * 30 seconds.
+	 */
+	@RequestMapping("/modelUpdate")
+	@ResponseBody
+	public synchronized DeferredResult<BeverageMachineModel> pollForModelUpdate(ModelMap model) {
+		final DeferredResult<BeverageMachineModel> awaitingResult = new DeferredResult<BeverageMachineModel>(30000L,getModel());
+		awaitingResult.onTimeout(new Runnable() {
+			@Override
+			public void run() {
+				awaitingResults.remove(awaitingResult);
+			}
+		});
+		awaitingResults.offer(awaitingResult);
+		return awaitingResult;
+	}
+	
+	/**
+	 * Called when the model has been updated. All asynchronous responses waiting for the model to change
+	 * are notified. 
+	 */
+	public synchronized void doModelUpdate() {
+		if(awaitingResults.isEmpty()) return;
+		DeferredResult<BeverageMachineModel> awaitingResult;
+		BeverageMachineModel model = getModel();
+		while((awaitingResult = awaitingResults.poll()) != null) {
+			awaitingResult.setResult(model);
+		}
+	}
+	
 }
