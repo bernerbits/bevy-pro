366a05adecc4341865b865ef5511209674b8693a
derek.berner <derek.berner@edointeractive.com>
2012-12-19 17:29:48 -0600

    Fixing platform-dependent unicode bug for cent sign

--- bevy-pro-frontend/src/main/java/com/bernerbits/vandy/bevypro/format/USCentsFormatter.java
+++ bevy-pro-frontend/src/main/java/com/bernerbits/vandy/bevypro/format/USCentsFormatter.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class USCentsFormatter implements MoneyFormatter {
 	@Override public String format(int cents) {
 		if(cents < 100) {
-			return cents + "¢";
+			return "" + cents + (char)0x00a2;
 		} else {
 			return "$" + new BigDecimal(cents).movePointLeft(2);
 		}
